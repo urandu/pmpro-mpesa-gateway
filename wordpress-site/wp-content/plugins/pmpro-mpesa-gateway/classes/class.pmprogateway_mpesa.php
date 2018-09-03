dd78d37533b4cd31ed9e880a3c5569a8f0147657
bildad namawa <bildadnamawa@gmail.com>
2018-09-03 04:22:39 +0300

    bildad.urandu changed file classes - auto-pilot mode

--- wordpress-site/wp-content/plugins/pmpro-mpesa-gateway/classes/class.pmprogateway_mpesa.php
+++ wordpress-site/wp-content/plugins/pmpro-mpesa-gateway/classes/class.pmprogateway_mpesa.php
@@ -239,7 +239,7 @@ class PMProGateway_mpesa extends PMProGateway
     static function pmpro_include_payment_information_fields($include)
     {
         //global vars
-        global $pmpro_requirebilling, $pmpro_show_discount_code, $discount_code, $CardType, $AccountNumber, $ExpirationMonth, $ExpirationYear, $current_user, $morder, $order_id;
+        global $pmpro_requirebilling, $pmpro_show_discount_code, $discount_code, $CardType, $AccountNumber, $ExpirationMonth, $ExpirationYear, $current_user, $morder, $order_id, $pmpro_level;
 
         //get accepted credit cards
         $pmpro_accepted_credit_cards = pmpro_getOption("accepted_credit_cards");
@@ -255,112 +255,25 @@ class PMProGateway_mpesa extends PMProGateway
                 <?php
 
                 print("<pre>");
-                print($order_id);
-                print_r($morder);
+                $amount =$pmpro_level->initial_payment;
+
                 print("</pre>");
                 ?>
-                <span class="pmpro_checkout-h3-msg"><?php printf(__('We Accept %s %s', 'paid-memberships-pro'), $current_user->ID, "dd"); ?></span>
+                <span class="pmpro_checkout-h3-name"><?php printf(__('To pay, go to mpesa and pay %s to till number %s'), $amount, "11111111"); ?></span>
             </h3>
             <?php $sslseal = pmpro_getOption("sslseal"); ?>
             <?php if (!empty($sslseal)) { ?>
             <div class="pmpro_checkout-fields-display-seal">
                 <?php } ?>
                 <div class="pmpro_checkout-fields">
-                    <?php
-                    $pmpro_include_cardtype_field = apply_filters('pmpro_include_cardtype_field', true);
-                    if ($pmpro_include_cardtype_field) { ?>
-                        <div class="pmpro_checkout-field pmpro_payment-card-type">
-                            <label for="CardType"><?php _e('Card Type', 'paid-memberships-pro'); ?></label>
-                            <select id="CardType" name="CardType"
-                                    class=" <?php echo pmpro_getClassForField("CardType"); ?>">
-                                <?php foreach ($pmpro_accepted_credit_cards as $cc) { ?>
-                                    <option value="<?php echo $cc ?>"
-                                            <?php if ($CardType == $cc) { ?>selected="selected"<?php } ?>><?php echo $cc ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                    <?php } ?>
                     <div class="pmpro_checkout-field pmpro_payment-account-number">
-                        <label for="AccountNumber"><?php _e('Card Number', 'paid-memberships-pro'); ?></label>
-                        <input id="AccountNumber" name="AccountNumber"
+                        <label for="AccountNumber"><?php _e('Phone Number', 'paid-memberships-pro'); ?></label>
+                        <input id="AccountNumber" name="msisdn"
                                class="input <?php echo pmpro_getClassForField("AccountNumber"); ?>" type="text"
-                               size="25" value="<?php echo esc_attr($AccountNumber) ?>" data-encrypted-name="number"
+                               size="25" value="<?php echo esc_attr($AccountNumber) ?>" data-encrypted-name="msisdn"
                                autocomplete="off"/>
                     </div>
-                    <div class="pmpro_checkout-field pmpro_payment-expiration">
-                        <label for="ExpirationMonth"><?php _e('Expiration Date', 'paid-memberships-pro'); ?></label>
-                        <select id="ExpirationMonth" name="ExpirationMonth"
-                                class=" <?php echo pmpro_getClassForField("ExpirationMonth"); ?>">
-                            <option value="01" <?php if ($ExpirationMonth == "01") { ?>selected="selected"<?php } ?>>
-                                01
-                            </option>
-                            <option value="02" <?php if ($ExpirationMonth == "02") { ?>selected="selected"<?php } ?>>
-                                02
-                            </option>
-                            <option value="03" <?php if ($ExpirationMonth == "03") { ?>selected="selected"<?php } ?>>
-                                03
-                            </option>
-                            <option value="04" <?php if ($ExpirationMonth == "04") { ?>selected="selected"<?php } ?>>
-                                04
-                            </option>
-                            <option value="05" <?php if ($ExpirationMonth == "05") { ?>selected="selected"<?php } ?>>
-                                05
-                            </option>
-                            <option value="06" <?php if ($ExpirationMonth == "06") { ?>selected="selected"<?php } ?>>
-                                06
-                            </option>
-                            <option value="07" <?php if ($ExpirationMonth == "07") { ?>selected="selected"<?php } ?>>
-                                07
-                            </option>
-                            <option value="08" <?php if ($ExpirationMonth == "08") { ?>selected="selected"<?php } ?>>
-                                08
-                            </option>
-                            <option value="09" <?php if ($ExpirationMonth == "09") { ?>selected="selected"<?php } ?>>
-                                09
-                            </option>
-                            <option value="10" <?php if ($ExpirationMonth == "10") { ?>selected="selected"<?php } ?>>
-                                10
-                            </option>
-                            <option value="11" <?php if ($ExpirationMonth == "11") { ?>selected="selected"<?php } ?>>
-                                11
-                            </option>
-                            <option value="12" <?php if ($ExpirationMonth == "12") { ?>selected="selected"<?php } ?>>
-                                12
-                            </option>
-                        </select>/<select id="ExpirationYear" name="ExpirationYear"
-                                          class=" <?php echo pmpro_getClassForField("ExpirationYear"); ?>">
-                            <?php for ($i = date_i18n("Y"); $i < date_i18n("Y") + 10; $i++) { ?>
-                                <option value="<?php echo $i ?>"
-                                        <?php if ($ExpirationYear == $i) { ?>selected="selected"<?php } ?>><?php echo $i ?></option>
-                            <?php } ?>
-                        </select>
-                    </div>
-                    <?php
-                    $pmpro_show_cvv = apply_filters("pmpro_show_cvv", true);
-                    if ($pmpro_show_cvv) { ?>
-                        <div class="pmpro_checkout-field pmpro_payment-cvv">
-                            <label for="CVV"><?php _e('CVV', 'paid-memberships-pro'); ?></label>
-                            <input class="input" id="CVV" name="cvv" type="text" size="4"
-                                   value="<?php if (!empty($_REQUEST['CVV'])) {
-                                       echo esc_attr(sanitize_text_field($_REQUEST['CVV']));
-                                   } ?>" class=" <?php echo pmpro_getClassForField("CVV"); ?>"
-                                   data-encrypted-name="cvv"/>
-                            <small>(<a href="javascript:void(0);"
-                                       onclick="javascript:window.open('<?php echo pmpro_https_filter(PMPRO_URL) ?>/pages/popup-cvv.html','cvv','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=600, height=475');"><?php _e("what's this?", 'paid-memberships-pro'); ?></a>)
-                            </small>
-                        </div>
-                    <?php } ?>
-                    <?php if ($pmpro_show_discount_code) { ?>
-                        <div class="pmpro_checkout-field pmpro_payment-discount-code">
-                            <label for="discount_code"><?php _e('Discount Code', 'paid-memberships-pro'); ?></label>
-                            <input class="input <?php echo pmpro_getClassForField("discount_code"); ?>"
-                                   id="discount_code" name="discount_code" type="text" size="20"
-                                   value="<?php echo esc_attr($discount_code) ?>"/>
-                            <input type="button" id="discount_code_button" name="discount_code_button"
-                                   value="<?php _e('Apply', 'paid-memberships-pro'); ?>"/>
-                            <p id="discount_code_message" class="pmpro_message" style="display: none;"></p>
-                        </div>
-                    <?php } ?>
+
                 </div> <!-- end pmpro_checkout-fields -->
                 <?php if (!empty($sslseal)) { ?>
                 <div class="pmpro_checkout-fields-rightcol pmpro_sslseal"><?php echo stripslashes($sslseal); ?></div>
@@ -389,7 +302,7 @@ class PMProGateway_mpesa extends PMProGateway
         unset($fields['bzipcode']);
         unset($fields['bcountry']);
         unset($fields['bphone']);
-        $fields['cvv'] = true;
+        $fields['msisdn'] = true;
         return $fields;
     }
 
