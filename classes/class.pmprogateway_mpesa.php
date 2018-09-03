1e26d17e63dc335dbce5c45a8261617447785e3b
bildad namawa <bildadnamawa@gmail.com>
2018-09-03 04:32:32 +0300

    bildad.urandu changed file classes - auto-pilot mode

--- classes/class.pmprogateway_mpesa.php
+++ classes/class.pmprogateway_mpesa.php
@@ -516,7 +516,7 @@ class PMProGateway_mpesa extends PMProGateway
 
         $post_url = apply_filters("pmpro_authorizenet_post_url", $post_url, $gateway_environment);
 
-        //what amount to authorize? just $1 to test.nnjhj
+        //what amount to authorize? just $1 to test.nnjh
         $amount = "1.00";
 
         //combine address
