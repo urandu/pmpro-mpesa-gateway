9056e529da6cd05ec235222562eaaee74aa6c651
bildad namawa <bildadnamawa@gmail.com>
2018-09-04 21:53:27 +0300

    bildad.urandu changed file classes - auto-pilot mode

--- classes/class.pmprogateway_mpesa.php
+++ classes/class.pmprogateway_mpesa.php
@@ -508,77 +508,77 @@ class PMProGateway_mpesa extends PMProGateway
 
     }
 
-    function void(&$order)
-    {
-        if(empty($order->payment_transaction_id))
-            return false;
-
-        if(empty($order->gateway_environment))
-            $gateway_environment = pmpro_getOption("gateway_environment");
-        else
-            $gateway_environment = $order->gateway_environment;
-        if($gateway_environment == "live")
-            $host = "secure.authorize.net";
-        else
-            $host = "test.authorize.net";
-
-        $path = "/gateway/transact.dll";
-        $post_url = "https://" . $host . $path;
-
-        $post_url = apply_filters("pmpro_authorizenet_post_url", $post_url, $gateway_environment);
-
-        $post_values = array(
-
-            // the API Login ID and Transaction Key must be replaced with valid values
-            "x_login"			=> pmpro_getOption("loginname"),
-            "x_tran_key"		=> pmpro_getOption("transactionkey"),
-
-            "x_version"			=> "3.1",
-            "x_delim_data"		=> "TRUE",
-            "x_delim_char"		=> "|",
-            "x_relay_response"	=> "FALSE",
-
-            "x_type"			=> "VOID",
-            "x_trans_id"			=> $order->payment_transaction_id
-            // Additional fields can be added here as outlined in the AIM integration
-            // guide at: http://developer.authorize.net
-        );
-
-        // This section takes the input fields and converts them to the proper format
-        // for an http post.  For example: "x_login=username&x_tran_key=a1B2c3D4"
-        $post_string = "";
-        foreach( $post_values as $key => $value )
-        { $post_string .= "$key=" . urlencode( str_replace("#", "%23", $value) ) . "&"; }
-        $post_string = rtrim( $post_string, "& " );
-
-        //curl
-        $request = curl_init($post_url); // initiate curl object
-        curl_setopt($request, CURLOPT_HEADER, 0); // set to 0 to eliminate header info from response
-        curl_setopt($request, CURLOPT_RETURNTRANSFER, 1); // Returns response data instead of TRUE(1)
-        curl_setopt($request, CURLOPT_POSTFIELDS, $post_string); // use HTTP POST to send form data
-        curl_setopt($request, CURLOPT_SSL_VERIFYPEER, FALSE); // uncomment this line if you get no gateway response.
-        $post_response = curl_exec($request); // execute curl post and store results in $post_response
-        // additional options may be required depending upon your server configuration
-        // you can find documentation on curl options at http://www.php.net/curl_setopt
-        curl_close ($request); // close curl object
-
-        // This line takes the response and breaks it into an array using the specified delimiting character
-        $response_array = explode($post_values["x_delim_char"],$post_response);
-        if($response_array[0] == 1)
-        {
-            $order->payment_transaction_id = $response_array[4];
-            $order->updateStatus("voided");
-            return true;
-        }
-        else
-        {
-            //$order->status = "error";
-            $order->errorcode = $response_array[2];
-            $order->error = $response_array[3];
-            $order->shorterror = $response_array[3];
-            return false;
-        }
-    }
+//    function void(&$order)
+//    {
+//        if(empty($order->payment_transaction_id))
+//            return false;
+//
+//        if(empty($order->gateway_environment))
+//            $gateway_environment = pmpro_getOption("gateway_environment");
+//        else
+//            $gateway_environment = $order->gateway_environment;
+//        if($gateway_environment == "live")
+//            $host = "secure.authorize.net";
+//        else
+//            $host = "test.authorize.net";
+//
+//        $path = "/gateway/transact.dll";
+//        $post_url = "https://" . $host . $path;
+//
+//        $post_url = apply_filters("pmpro_authorizenet_post_url", $post_url, $gateway_environment);
+//
+//        $post_values = array(
+//
+//            // the API Login ID and Transaction Key must be replaced with valid values
+//            "x_login"			=> pmpro_getOption("loginname"),
+//            "x_tran_key"		=> pmpro_getOption("transactionkey"),
+//
+//            "x_version"			=> "3.1",
+//            "x_delim_data"		=> "TRUE",
+//            "x_delim_char"		=> "|",
+//            "x_relay_response"	=> "FALSE",
+//
+//            "x_type"			=> "VOID",
+//            "x_trans_id"			=> $order->payment_transaction_id
+//            // Additional fields can be added here as outlined in the AIM integration
+//            // guide at: http://developer.authorize.net
+//        );
+//
+//        // This section takes the input fields and converts them to the proper format
+//        // for an http post.  For example: "x_login=username&x_tran_key=a1B2c3D4"
+//        $post_string = "";
+//        foreach( $post_values as $key => $value )
+//        { $post_string .= "$key=" . urlencode( str_replace("#", "%23", $value) ) . "&"; }
+//        $post_string = rtrim( $post_string, "& " );
+//
+//        //curl
+//        $request = curl_init($post_url); // initiate curl object
+//        curl_setopt($request, CURLOPT_HEADER, 0); // set to 0 to eliminate header info from response
+//        curl_setopt($request, CURLOPT_RETURNTRANSFER, 1); // Returns response data instead of TRUE(1)
+//        curl_setopt($request, CURLOPT_POSTFIELDS, $post_string); // use HTTP POST to send form data
+//        curl_setopt($request, CURLOPT_SSL_VERIFYPEER, FALSE); // uncomment this line if you get no gateway response.
+//        $post_response = curl_exec($request); // execute curl post and store results in $post_response
+//        // additional options may be required depending upon your server configuration
+//        // you can find documentation on curl options at http://www.php.net/curl_setopt
+//        curl_close ($request); // close curl object
+//
+//        // This line takes the response and breaks it into an array using the specified delimiting character
+//        $response_array = explode($post_values["x_delim_char"],$post_response);
+//        if($response_array[0] == 1)
+//        {
+//            $order->payment_transaction_id = $response_array[4];
+//            $order->updateStatus("voided");
+//            return true;
+//        }
+//        else
+//        {
+//            //$order->status = "error";
+//            $order->errorcode = $response_array[2];
+//            $order->error = $response_array[3];
+//            $order->shorterror = $response_array[3];
+//            return false;
+//        }
+//    }
 
     function charge(&$order)
     {
@@ -607,6 +607,16 @@ class PMProGateway_mpesa extends PMProGateway
         $tax = $order->getTax(true);
         $amount = round((float)$order->subtotal + (float)$tax, 2);
 
+
+
+        //check db for transaction associated with phone_number
+        global $wpdb;
+
+        //to use account_number for paybills.
+        $mpesa_msisdn = $order->mpesa_msisdn;
+        $table_name = $wpdb->prefix . 'mpesa_pmpro';
+        $total_amount_paid_by_msisdn = $wpdb->get_var( "SELECT SUM(amount) AS total_amount FROM $table_name WHERE msisdn=$mpesa_msisdn AND order_id=-1;" );
+        $order->mpesa_payment_amount
         //combine address
         $address = $order->Address1;
         if(!empty($order->Address2))
