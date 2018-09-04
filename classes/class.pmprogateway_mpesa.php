eddccf6e4a569cee00ef72ba9456e95ed354483f
bildad namawa <bildadnamawa@gmail.com>
2018-09-04 22:08:27 +0300

    bildad.urandu changed file classes - auto-pilot mode

--- classes/class.pmprogateway_mpesa.php
+++ classes/class.pmprogateway_mpesa.php
@@ -595,91 +595,15 @@ class PMProGateway_mpesa extends PMProGateway
             $order->updateStatus("success");
             return true;
         } else {
-
-        }
-        //combine address
-        $address = $order->Address1;
-        if (!empty($order->Address2))
-            $address .= "\n" . $order->Address2;
-
-        //customer stuff
-        $customer_email = $order->Email;
-        $customer_phone = $order->billing->phone;
-
-        if (!isset($order->membership_level->name))
-            $order->membership_level->name = "";
-
-        $post_values = array(
-
-            // the API Login ID and Transaction Key must be replaced with valid values
-            "x_login" => pmpro_getOption("loginname"),
-            "x_tran_key" => pmpro_getOption("transactionkey"),
-
-            "x_version" => "3.1",
-            "x_delim_data" => "TRUE",
-            "x_delim_char" => "|",
-            "x_relay_response" => "FALSE",
-
-            "x_type" => "AUTH_CAPTURE",
-            "x_method" => "CC",
-            "x_card_type" => $order->cardtype,
-            "x_card_num" => $order->accountnumber,
-            "x_exp_date" => $order->ExpirationDate,
-
-            "x_amount" => $amount,
-            "x_tax" => $tax,
-            "x_description" => $order->membership_level->name . " Membership",
-
-            "x_first_name" => $order->FirstName,
-            "x_last_name" => $order->LastName,
-            "x_address" => $address,
-            "x_city" => $order->billing->city,
-            "x_state" => $order->billing->state,
-            "x_zip" => $order->billing->zip,
-            "x_country" => $order->billing->country,
-            "x_invoice_num" => $order->code,
-            "x_phone" => $customer_phone,
-            "x_email" => $order->Email
-
-            // Additional fields can be added here as outlined in the AIM integration
-            // guide at: http://developer.authorize.net
-        );
-
-        if (!empty($order->CVV2))
-            $post_values["x_card_code"] = $order->CVV2;
-
-        // This section takes the input fields and converts them to the proper format
-        // for an http post.  For example: "x_login=username&x_tran_key=a1B2c3D4"
-        $post_string = "";
-        foreach ($post_values as $key => $value) {
-            $post_string .= "$key=" . urlencode(str_replace("#", "%23", $value)) . "&";
-        }
-        $post_string = rtrim($post_string, "& ");
-
-        //curl
-        $request = curl_init($post_url); // initiate curl object
-        curl_setopt($request, CURLOPT_HEADER, 0); // set to 0 to eliminate header info from response
-        curl_setopt($request, CURLOPT_RETURNTRANSFER, 1); // Returns response data instead of TRUE(1)
-        curl_setopt($request, CURLOPT_POSTFIELDS, $post_string); // use HTTP POST to send form data
-        curl_setopt($request, CURLOPT_SSL_VERIFYPEER, FALSE); // uncomment this line if you get no gateway response.
-        $post_response = curl_exec($request); // execute curl post and store results in $post_response
-        // additional options may be required depending upon your server configuration
-        // you can find documentation on curl options at http://www.php.net/curl_setopt
-        curl_close($request); // close curl object
-
-        // This line takes the response and breaks it into an array using the specified delimiting character
-        $response_array = explode($post_values["x_delim_char"], $post_response);
-        if ($response_array[0] == 1) {
-            $order->payment_transaction_id = $response_array[6];
-            $order->updateStatus("success");
-            return true;
-        } else {
+            // the amount is not fully paid return error to checkout page
             //$order->status = "error";
-            $order->errorcode = $response_array[2];
-            $order->error = $response_array[3];
-            $order->shorterror = $response_array[3];
+            $order->errorcode = "transaction failed 1";
+            $order->error = "transaction failed 2";
+            $order->shorterror = "transaction failed 3";
             return false;
+
         }
+        
     }
 
     function subscribe(&$order)
