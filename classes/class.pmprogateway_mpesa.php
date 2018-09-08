b3c73ef64b08ff255fe67fa75952154bbdefa2bd
bildad namawa <bildadnamawa@gmail.com>
2018-09-08 09:21:43 +0300

    bildad.urandu changed file classes - auto-pilot mode

--- classes/class.pmprogateway_mpesa.php
+++ classes/class.pmprogateway_mpesa.php
@@ -621,7 +621,7 @@ class PMProGateway_mpesa extends PMProGateway
 
             return false;
         }else{
-            
+
         }
         $total_amount_paid_by_msisdn = $wpdb->get_var("SELECT SUM(amount) AS total_amount FROM $table_name WHERE msisdn=$mpesa_msisdn AND order_id=-1;");
 
