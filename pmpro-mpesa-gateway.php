90bf3bd7f43b4cde4a7f454c36ea0d4d0432f57c
bildad namawa <bildadnamawa@gmail.com>
2018-11-02 01:17:43 +0300

    bildad.urandu changed file pmpro-mpesa-gateway.php___jb_old___ pmpro-mpesa-gateway.php pmpro-mpesa-gateway.php___jb_tmp___ - auto-pilot mode

--- pmpro-mpesa-gateway.php
+++ pmpro-mpesa-gateway.php
@@ -7,7 +7,7 @@ Version: .1
 
 define("PMPRO_MPESAGATEWAY_DIR", dirname(__FILE__));
 
-//load payment gateway class
+//load payment gateway class small change
 require_once(PMPRO_MPESAGATEWAY_DIR . "/classes/class.pmprogateway_mpesa.php");
 register_activation_hook( __FILE__, 'mpesa_install' );
 add_action('init', 'pmpro_mpesa_ipn_listener');
